a86cb98bf11af04057c0bc67ef2543836b59b8bc
JuHyeonAh <jha08130930@gmail.com>
2025-05-27 16:23:36 +0900

    entity 설정 완료

--- src/main/java/com/capstone2/capstone2/domain/chat/repository/ChatRoomMember.java
+++
@@ -1,7 +0,0 @@
-package com.capstone2.capstone2.domain.chat.repository;
-
-import com.capstone2.capstone2.domain.chat.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomMember extends JpaRepository<ChatRoomMember, Long> {
-}
