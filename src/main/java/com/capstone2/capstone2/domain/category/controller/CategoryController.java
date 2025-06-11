36bf4d91f63de9a78bcb60ccab3f6e52bbfbe551
JuHyeonAh <jha08130930@gmail.com>
2025-06-11 10:03:21 +0900

    병합 및 기존 개발 하던 카테고리 API 주석 처리

--- src/main/java/com/capstone2/capstone2/domain/category/controller/CategoryController.java
+++ src/main/java/com/capstone2/capstone2/domain/category/controller/CategoryController.java
@@ -19,24 +19,24 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    @Operation(summary = "카테고리 목록 조회", description = "대분류와 중분류를 모두 조회합니다.")
-    @GetMapping
-    public ApiResponse<List<CategoryResponse.MainCategoryDTO>> getAllCategories() {
-        List<CategoryResponse.MainCategoryDTO> result = categoryService.getAllCategories();
-        return ApiResponse.onSuccess(SuccessStatus.CATEGORY_FETCH_ALL_OK, result);
-    }
-
-    @GetMapping("/main")
-    public ApiResponse<List<CategoryResponse.MainCategoryDTO>> getMainCategories() {
-        List<CategoryResponse.MainCategoryDTO> result = categoryService.getMainCategories();
-        return ApiResponse.onSuccess(SuccessStatus.CATEGORY_FETCH_MAIN_OK, result);
-    }
-
-    @GetMapping("/main/{mainCategoryId}/sub")
-    public ApiResponse<List<CategoryResponse.SubCategoryDTO>> getSubCategories(@PathVariable Long mainCategoryId) {
-        List<CategoryResponse.SubCategoryDTO> result = categoryService.getSubCategoriesByMainId(mainCategoryId);
-        return ApiResponse.onSuccess(SuccessStatus.CATEGORY_FETCH_SUB_OK, result);
-
-
-    }
+//    @Operation(summary = "카테고리 목록 조회", description = "대분류와 중분류를 모두 조회합니다.")
+//    @GetMapping
+//    public ApiResponse<List<CategoryResponse.MainCategoryDTO>> getAllCategories() {
+//        List<CategoryResponse.MainCategoryDTO> result = categoryService.getAllCategories();
+//        return ApiResponse.onSuccess(SuccessStatus.CATEGORY_FETCH_ALL_OK, result);
+//    }
+//
+//    @GetMapping("/main")
+//    public ApiResponse<List<CategoryResponse.MainCategoryDTO>> getMainCategories() {
+//        List<CategoryResponse.MainCategoryDTO> result = categoryService.getMainCategories();
+//        return ApiResponse.onSuccess(SuccessStatus.CATEGORY_FETCH_MAIN_OK, result);
+//    }
+//
+//    @GetMapping("/main/{mainCategoryId}/sub")
+//    public ApiResponse<List<CategoryResponse.SubCategoryDTO>> getSubCategories(@PathVariable Long mainCategoryId) {
+//        List<CategoryResponse.SubCategoryDTO> result = categoryService.getSubCategoriesByMainId(mainCategoryId);
+//        return ApiResponse.onSuccess(SuccessStatus.CATEGORY_FETCH_SUB_OK, result);
+//
+//
+//    }
 }
